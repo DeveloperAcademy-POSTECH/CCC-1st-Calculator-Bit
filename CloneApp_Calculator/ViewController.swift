8acca5d16e382193f5d6f023059887d18fbef3d0
yeongwoo_Cho <jyy0223@naver.com>
2022-05-30 15:27:34 +0900

    add self view

--- CloneApp_Calculator/ViewController.swift
+++ CloneApp_Calculator/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func loadView() {
+        super.loadView()
         
-        self.view.backgroundColor = .orange
+        self.view = UIView()
+        self.setView()
+    }
+    
+    private func setView() {
+        guard let view = self.view else { return }
+        view.backgroundColor = .orange
     }
 }
